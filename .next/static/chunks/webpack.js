1f9d5b65fe95c5f962f4c8326ba556c396b63ef1
Juliana Fogg <julianafogg@gmail.com>
2020-09-09 00:43:26 -0400

    add store to shopify

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "05d004e8b04cafe793cf";
+/******/ 	var hotCurrentHash = "109b498e6490cc17e557";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
