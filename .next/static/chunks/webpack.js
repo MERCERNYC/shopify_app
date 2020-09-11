58f3a908fd28354e1926ad208fc9f01ec670088c
Juliana Fogg <julianafogg@gmail.com>
2020-09-10 23:28:05 -0400

    add annotated layout

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "29948af3087fc282381c";
+/******/ 	var hotCurrentHash = "f6c7c665cb465eb63264";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
